5bc885b71ad46c756677066869e3025c78ec2a38
Kacper Sadecki <kacpersadik@gmail.com>
2025-04-14 00:18:02 +0200

    ale jaja działa

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
@@ -15,8 +17,8 @@ namespace Gra2D
         public const int LAS = 1;     // las
         public const int LAKA = 2;     // łąka
         public const int SKALA = 3;    // skały
-        public const int JAGODY = 4;   // DODANE: jagody (jedzenie)
-        public const int ILE_TERENOW = 5;   // DODANE: zwiększono o 1
+        public const int JAGODY = 4;   // jagody (jedzenie)
+        public const int ILE_TERENOW = 5;
 
         private int[,] mapa;
         private int szerokoscMapy;
@@ -29,15 +31,16 @@ namespace Gra2D
         private int pozycjaGraczaY = 0;
         private Image obrazGracza;
         private int iloscDrewna = 0;
-        private int energia = 100; // DODANE: energia gracza
-        private DispatcherTimer timerEnergii; // DODANE: timer energii
+        private int energia = 100;
+        private DispatcherTimer timerEnergii;
+        private int liczbaRund = 1;
+        private int liczbaDrzewNaMapie = 0;
 
         public MainWindow()
         {
             InitializeComponent();
             WczytajObrazyTerenu();
 
-            // Inicjalizacja obrazka gracza
             obrazGracza = new Image
             {
                 Width = RozmiarSegmentu,
@@ -45,35 +48,70 @@ namespace Gra2D
                 Source = new BitmapImage(new Uri("Zdjecia/gracz.png", UriKind.Relative))
             };
 
-            // DODANE: Start timera energii
             StartTimerEnergii();
+            GenerujLosowaMape(20, 15);
         }
 
-        // DODANE: Generowanie losowej mapy
+        private void AnimujZmianeRundy()
+        {
+            var brush = new SolidColorBrush(Colors.DarkGreen);
+            EtykietaRundy.Foreground = brush;
+
+            var animation = new ColorAnimation
+            {
+                From = Colors.DarkGreen,
+                To = Colors.Red,
+                Duration = TimeSpan.FromSeconds(0.5),
+                AutoReverse = true
+            };
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+
+        private void SprawdzCzyKoniecRundy()
+        {
+            if (iloscDrewna >= liczbaDrzewNaMapie)
+            {
+                timerEnergii.Stop();
+                MessageBox.Show($"GRATULACJE, PRZEJDŹ DO NASTĘPNEJ RUNDY!\n(Aktualna runda: {liczbaRund})");
+                liczbaRund++;
+                EtykietaRundy.Content = $"Runda: {liczbaRund}";
+                AnimujZmianeRundy();
+                GenerujLosowaMape(20, 15);
+            }
+        }
+
         private void GenerujLosowaMape(int szerokosc, int wysokosc)
         {
             Random rand = new Random();
             mapa = new int[wysokosc, szerokosc];
             szerokoscMapy = szerokosc;
             wysokoscMapy = wysokosc;
+            liczbaDrzewNaMapie = 0;
 
             for (int y = 0; y < wysokosc; y++)
             {
                 for (int x = 0; x < szerokosc; x++)
                 {
-                    // 60% łąka, 25% las, 10% skały, 5% jagody
                     int los = rand.Next(100);
                     if (los < 60) mapa[y, x] = LAKA;
-                    else if (los < 85) mapa[y, x] = LAS;
+                    else if (los < 85)
+                    {
+                        mapa[y, x] = LAS;
+                        liczbaDrzewNaMapie++;
+                    }
                     else if (los < 95) mapa[y, x] = SKALA;
                     else mapa[y, x] = JAGODY;
                 }
             }
 
             RysujMape();
+            iloscDrewna = 0;
+            EtykietaDrewna.Content = $"Drewno: {iloscDrewna}/{liczbaDrzewNaMapie}";
+            energia = 100;
+            PasekEnergii.Value = 100;
+            timerEnergii.Start();
         }
 
-        // DODANE: Rysowanie mapy (wyodrębnione z WczytajMape)
         private void RysujMape()
         {
             SiatkaMapy.Children.Clear();
@@ -95,7 +133,7 @@ namespace Gra2D
                     {
                         Width = RozmiarSegmentu,
                         Height = RozmiarSegmentu,
-                        Source = (mapa[y, x] >= 1 && mapa[y, x] < ILE_TERENOW) ? obrazyTerenu[mapa[y, x]] : null
+                        Source = obrazyTerenu[mapa[y, x]]
                     };
                     Grid.SetRow(obraz, y);
                     Grid.SetColumn(obraz, x);
@@ -109,19 +147,12 @@ namespace Gra2D
             pozycjaGraczaX = 0;
             pozycjaGraczaY = 0;
             AktualizujPozycjeGracza();
-
-            iloscDrewna = 0;
-            EtykietaDrewna.Content = "Drewno: " + iloscDrewna;
-            energia = 100;
-            PasekEnergii.Value = 100;
-            timerEnergii.Start();
         }
 
-        // DODANE: Timer energii
         private void StartTimerEnergii()
         {
             timerEnergii = new DispatcherTimer();
-            timerEnergii.Interval = TimeSpan.FromSeconds(5);
+            timerEnergii.Interval = TimeSpan.FromSeconds(2.5);
             timerEnergii.Tick += (s, e) =>
             {
                 energia -= 5;
@@ -129,7 +160,7 @@ namespace Gra2D
                 if (energia <= 0)
                 {
                     timerEnergii.Stop();
-                    MessageBox.Show("Koniec gry! Zabrakło energii.");
+                    MessageBox.Show($"Koniec gry! Zabrakło energii. (Ukończone rundy: {liczbaRund - 1})");
                 }
             };
             timerEnergii.Start();
@@ -161,22 +192,27 @@ namespace Gra2D
 
             if (nowyX >= 0 && nowyX < szerokoscMapy && nowyY >= 0 && nowyY < wysokoscMapy)
             {
-                if (mapa[nowyY, nowyX] != SKALA)
+                if (mapa[nowyY, nowyX] == SKALA)
                 {
-                    pozycjaGraczaX = nowyX;
-                    pozycjaGraczaY = nowyY;
-                    AktualizujPozycjeGracza();
+                    WejdzNaSkale();
                 }
+
+                pozycjaGraczaX = nowyX;
+                pozycjaGraczaY = nowyY;
+                AktualizujPozycjeGracza();
             }
 
-            if (e.Key == Key.C && mapa[pozycjaGraczaY, pozycjaGraczaX] == LAS)
+            // Ścinanie drzew - klawisz E
+            if (e.Key == Key.E && mapa[pozycjaGraczaY, pozycjaGraczaX] == LAS)
             {
                 mapa[pozycjaGraczaY, pozycjaGraczaX] = LAKA;
                 tablicaTerenu[pozycjaGraczaY, pozycjaGraczaX].Source = obrazyTerenu[LAKA];
                 iloscDrewna++;
-                EtykietaDrewna.Content = "Drewno: " + iloscDrewna;
+                EtykietaDrewna.Content = $"Drewno: {iloscDrewna}/{liczbaDrzewNaMapie}";
+                SprawdzCzyKoniecRundy();
             }
 
+            // Zbieranie jagód - klawisz E
             if (e.Key == Key.E && mapa[pozycjaGraczaY, pozycjaGraczaX] == JAGODY)
             {
                 energia = Math.Min(100, energia + 20);
@@ -186,12 +222,26 @@ namespace Gra2D
             }
         }
 
+        private void WejdzNaSkale()
+        {
+            int kara = energia / 2;
+            energia -= kara;
+            PasekEnergii.Value = energia;
+
+            if (energia <= 0)
+            {
+                timerEnergii.Stop();
+                MessageBox.Show($"Koniec gry! Wspinaczka wyczerpała Twoją energię. (Ukończone rundy: {liczbaRund - 1})");
+            }
+        }
+
         private void WczytajMape_Click(object sender, RoutedEventArgs e)
         {
-            // DODANE: Generuj losową mapę 20x15 zamiast wczytywać z pliku
+            liczbaRund = 1;
+            EtykietaRundy.Content = $"Runda: {liczbaRund}";
+            AnimujZmianeRundy();
             GenerujLosowaMape(20, 15);
         }
     }
 }
-
 
